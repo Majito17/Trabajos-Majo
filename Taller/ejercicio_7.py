5ff29213b0fc109ce37d7468f777cb37095039f2
Maria Jose Gomez <majo1701ven@gmail.com>
2022-02-04 16:34:52 -0500

    Gracias
    Subir

---
+++ Taller/ejercicio_7.py
@@ -0,0 +1,12 @@
+"""
+datos de entrada
+metros-->m-->float
+"""
+#entrada
+m=float(input("Ingrese medida en metros: "))
+#caja negra
+inch=(m*39.27)
+feet=inch/12
+#salida
+print(inch,"En pulgadas equivale: ")
+print(feet,"En pies equivale: ")
