4eb9bb7d81d4e80fec5c4cb376f9e6a9da30eee1
Maria Jose Gomez <majo1701ven@gmail.com>
2022-02-04 16:38:27 -0500

    Taller
    J

---
+++ Taller/ejercicio_2.py
@@ -0,0 +1,12 @@
+"""
+Datos de entrada
+capital-->cap-->int
+Datos de salida
+ganancia_mes-->gam-->float
+"""
+#Entradas
+cap=int(input("digite su capital: "))
+#Caja negra
+gam=(cap*0.2)/100
+#Salidas
+print("Su ganancia mensual es: ", gam)
